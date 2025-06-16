6396d4eb3dbc5adc2acc59b66309da814fca1368
unknown <nurinamaizurah325@gmail.com>
2025-06-16 21:18:10 +0800

    tambah header

--- loginPage.php
+++ loginPage.php
@@ -117,14 +117,64 @@
       margin: 0 10px;
       cursor: pointer;
     }
+
+    .navbar {
+        background-color: #660066;
+        position: sticky;
+        top: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 10px;
+        height: 60px;
+      }
+
+      .navbar .logo {
+        height: 60px;
+      }
+
+      .navbar ul {
+        list-style: none;
+        display: flex;
+        margin: 0;
+        padding: 0;
+      }
+
+      .navbar li {
+        margin-left: 10px;
+      }
+
+      .navbar a {
+        text-decoration: none;
+        color: white;
+        padding: 14px 16px;
+        display: block;
+        font-size: 14px;
+        font-weight: bold;
+        text-transform: uppercase;
+      }
+
+      .navbar a:hover {
+        background-color: #990099;
+      }
   </style>
 </head>
 <body>
-
-  <div class="header">
-    <h1>WELCOME TO STUDY HIVE</h1>
-  </div>
-
+  <section>
+    <div class="navbar">
+          <img src="images/whiteLogo.png" alt="Logo" class="logo">
+          <ul>
+          <li><a href="#namafile">Features</a></li>
+          <li><a href="#namafile">Help</a></li>
+          <li><a href="#namafile">Contact Us</a></li>
+          <li><a href="loginPage.php">Login</a></li>
+          <li><a href="signupPage.php">Sign Up</a></li>
+        </ul>
+        </div>
+        <div class="topic">
+          <h2>WELCOME TO STUDY HIVE</h2>
+          
+        </div>
   <div class="container">
     <div class="login-box">
       <h2>Log in</h2>
@@ -156,6 +206,6 @@
       </div>
     </div>
   </div>
-
+</section>
 </body>
 </html>
