db6aa5fad3013904f87f51d2c33c7cb484901862
Nurina Maizurah <nurinamaizurah325@gmail.com>
2025-06-22 01:03:47 +0800

    tambah header studyhive

--- forgotPassword.php
+++ forgotPassword.php
@@ -58,6 +58,75 @@ if (isset($_POST['reset_password'])) {
       background-color: #f2f2f2;
     }
 
+    .navbar {
+      background-color: #660066;
+      position: sticky;
+      top: 0;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 10px;
+      height: 60px;
+    }
+
+    .navbar .logo {
+      height: 60px;
+    }
+
+    .navbar ul {
+      list-style: none;
+      display: flex;
+      margin: 0;
+      padding: 0;
+    }
+
+    .navbar li {
+      margin-left: 10px;
+    }
+
+    .navbar a {
+      text-decoration: none;
+      color: white;
+      padding: 14px 16px;
+      display: block;
+      font-size: 14px;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+
+    .navbar a:hover {
+      background-color: #990099;
+    }
+
+    .topic {
+      background-color: #ec97ec;
+      font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+      font-size: 230%;
+      text-decoration: none;
+      color: #5e1b5e;
+      text-align: center;
+      height: 400px;
+      padding-top: 5px;
+      padding-bottom: 5px;
+    }
+
+    .topic img {
+      width: 245px;
+      height: auto;
+      margin-bottom: 5px;
+      margin-top: 5px;
+    }
+
+    .back-btn {
+      display: inline-block;
+      margin-top: 20px;
+      margin-left: 20px;
+      font-size: 16px;
+      color: #660066;
+      text-decoration: none;
+      font-weight: bold;
+    }
+
     .container {
       display: flex;
       justify-content: center;
@@ -127,6 +196,17 @@ if (isset($_POST['reset_password'])) {
   </style>
 </head>
 <body>
+    <section?>
+      <div class="navbar">
+            <img src="images/whiteLogo.png" alt="Logo" class="logo">
+            <ul>
+            <li><a href="#namafile">Features</a></li>
+            <li><a href="#namafile">Help</a></li>
+            <li><a href="#namafile">Contact Us</a></li>
+            <li><a href="loginPage.php">Login</a></li>
+            <li><a href="signupPage.php">Sign Up</a></li>
+          </ul>
+        </div>
   <div class="container">
     <div class="box">
       <h2>Reset Password</h2>
@@ -159,5 +239,6 @@ if (isset($_POST['reset_password'])) {
       <?php endif; ?>
     </div>
   </div>
+</section>
 </body>
 </html>
