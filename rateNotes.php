a676b606d546fb44dd87d9bcecfbce0c482b80d0
Nurain Batrisyia <nurainazwan27@gmail.com>
2025-06-22 18:43:30 +0800

    add rateNotes.php

---
+++ rateNotes.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rate Notes</title>
+    <style>
+        .back-btn { 
+        margin: 20px; 
+        display: inline-block; 
+        font-size: 16px; 
+        color: #660066; 
+        text-decoration: none; 
+        font-weight: bold; 
+        }
+
+        body { 
+            font-family: Arial; 
+        }
+
+        .review { 
+            border-bottom: 1px solid #ccc; 
+            margin-bottom: 15px; 
+            padding-bottom: 10px; 
+        }
+
+        h1 { 
+            font-size: 60px; 
+            text-align: center; 
+            color: #4b004b; 
+        }
+
+    </style>
+</head>
+<body>
+    <?php
+        include('head.php');
+    ?>
+
+    <a class="back-btn" href="download.php">&larr; Back</a>
+    <h1>Review Notes</h1>
+    <form method="POST" >
+        
+    </form>
+
+
+</body>
+</html>
