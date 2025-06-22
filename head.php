32ddc1cff39e9f247ef08b25327a93bcc7ecdcd4
Nurina Maizurah <nurinamaizurah325@gmail.com>
2025-06-22 23:28:44 +0800

    adjust nama

--- head.php
+++ head.php
@@ -17,7 +17,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Head</title>
+  <title>Profile</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
   <style>
