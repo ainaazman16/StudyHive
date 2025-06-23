2840b51a0fbf706626ef195c7f553335c69f749e
Nurina Maizurah <nurinamaizurah325@gmail.com>
2025-06-23 14:23:20 +0800

    add placeholder

--- signupPage.php
+++ signupPage.php
@@ -183,13 +183,13 @@
     <h2>Create Your Account</h2>
 
     <label for="user_Fname">Full Name:</label>
-    <input type="text" name="user_Fname" id="user_Fname" required>
+    <input type="text" name="user_Fname" id="user_Fname" placeholder="Insert your full name" required>
 
     <label for="email">Email Address:</label>
-    <input type="email" name="email" id="email" required>
+    <input type="email" name="email" id="email" placeholder="Insert your email address" required>
 
     <label for="phone">Phone Number:</label>
-    <input type="number" name="phone" id="phone" required>
+    <input type="number" name="phone" id="phone" placeholder="Insert your phone number" required>
 
     <label>Gender:</label>
     <div class="gender-options">
@@ -198,10 +198,10 @@
     </div>
 
     <label for="user_Name">Username:</label>
-    <input type="text" name="user_Name" id="user_Name" required>
+    <input type="text" name="user_Name" id="user_Name" placeholder="Insert your username" required>
 
     <label for="password">Password:</label>
-    <input type="password" name="password" id="password" required>
+    <input type="password" name="password" id="password" placeholder="Insert your password" required>
 
     <label for="profile_picture">Profile Picture:</label>
     <input type="file" name="profile_picture" id="profile_picture" required>
